bc7876d77d4a64b1b817a0d997d4dac39686be14
Timothy Mostert <resident@hotmail.co.za>
2022-01-20 15:36:20 +0800

    initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="calculator">
+    <section class="display">
+      {{ counter }}
+    </section>
+    <section class="inputs">
+      <div class="default-button" @click="decrementCounter()">-</div>
+      <div class="default-button" @click="incrementCounter()">+</div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Calculator",
+  setup() {
+    let counter = ref(1);
+
+    const incrementCounter = () => {
+      counter.value++;
+    };
+
+    const decrementCounter = () => {
+      if (counter.value > 0) {
+        counter.value--;
+      }
+    };
+
+    return {
+      counter,
+      incrementCounter,
+      decrementCounter,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.calculator {
+  padding: 1.5em 1em;
+  background: #2c2c2c;
+  width: 400px;
+  height: 600px;
+  .display {
+    background-color: beige;
+    width: fill-available;
+    padding: 0.75em;
+    text-align: left;
+    font-weight: bold;
+    margin-bottom: 0.75em;
+    border: 2px solid white;
+  }
+  .inputs {
+    display: flex;
+    align-items: center;
+    .default-button {
+      background: lightgray;
+      padding: 0.75em;
+      margin-right: 0.75em;
+      min-width: 120px;
+      border: 2px solid hsl(0, 1%, 27%);
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Calculator from "@/components/Calculator.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Calculator,
+  },
+};
 </script>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  justify-content: center;
+  padding: 2em;
+}
+</style>
